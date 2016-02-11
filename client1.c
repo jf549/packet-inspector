be8406cf5b9255abd6f61621206323c89ab7d507
Jamie Fox <jf549@cam.ac.uk>
2016-02-11 17:32:28 +0000

    clean up files

--- client1.c
+++ client1.c
@@ -1,4 +1,3 @@
-/* client.c */
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
--- server1.c
+++ server1.c
@@ -1,4 +1,3 @@
-/* server.c */
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
